6df08bdde333554e99205d150cc293ae60633c2f
Jesper <jesper@scofu.com>
2022-04-03 21:26:47 +0200

    add @Module annotation

--- common-json/src/main/java/com/scofu/common/json/JsonModule.java
+++ common-json/src/main/java/com/scofu/common/json/JsonModule.java
@@ -1,11 +1,13 @@
 package com.scofu.common.json;
 
 import com.scofu.common.inject.AbstractFeatureModule;
+import com.scofu.common.inject.annotation.Module;
 import com.scofu.common.json.internal.InternalJsonModule;
 
 /**
  * Json module. Binds common adapters.
  */
+@Module
 public class JsonModule extends AbstractFeatureModule {
 
   @Override
